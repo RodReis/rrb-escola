cfde7c9e761a2c51f278d792b7d1ddd7f7aa245c
Rodrigo Reis <rodreisb@gmail.com>
2026-05-20 18:49:30 -0300

    build: disable ESLint during Vercel build (plugin missing in prod deps)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
       { protocol: "http",  hostname: "127.0.0.1", port: "55421", pathname: "/storage/v1/object/public/**" },
